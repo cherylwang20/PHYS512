29a6b2ddbc768ea3e7adac0d4ee1a473afa299cc
Huiyi Wang <huiyi.wang@mail.mcgill.ca>
2021-09-15 19:22:13 -0400

    hh

--- p3.py
+++ p3.py
@@ -1,13 +1,12 @@
 import numpy as np
+from scipy import interpolate
 
 dat = np.loadtxt('lakeshore.txt')
-T_true = dat[:, 0] # for later comparsion
-V_true = dat[:, 1]
+
 
 def lakeshore(V, data):
-    T_in = data[:, 0]
+    T_in = data[:, 0] # for later comparsion
     V_in = data[:, 1]
-
 
     if isinstance(V,float) == True:
         ind = np.argmin(V_in > V)  # find the argument of the smallest number greater than V
@@ -37,8 +36,31 @@ def lakeshore(V, data):
                 T_out[i] = np.polyval(p, V[i])
     return T_out
 
-V_i = [0.58,0.130,1.45,1.3,1.25666]
-unc = np.std(lakeshore(V_true,dat) - T_true) # find the approximate error of the terms
 
-print('The Temperature at', V_i, 'are',lakeshore(V_i, dat),'correspondingly with uncertainty.', unc)
+
+def lakeshore_c(V, data):
+    V_in = data[:, 1]
+    T_in = data[:, 0] # for later comparsion
+
+    spln = interpolate.splrep(V_in[::-1], T_in[::-1])
+    TT_c = interpolate.splev(V, spln)    
+    return TT_c
+
+def ndiff(fun,x):
+    dx = np.zeros(len(x))
+    deriv = np.zeros(len(x))
+    error = np.zeros((len(x)))
+    error2 = np.zeros(len(x))
+    for i in range(len(x)):
+        eps = 7 * 10 ** (-17)  # what should be the optimized eps???
+        dx[i] = (eps) ** (1 / 3) * x[i]
+        deriv[i] = (fun(x[i] + dx[i],dat) - fun(x[i] - dx[i],dat))/(2*dx[i])
+    return deriv
+
+   
+V_i = [0.6,0.8]
+print(lakeshore_c(V_i, dat))
+ndiff(lakeshore_c,V_i)
+
+print('The Temperature at', V_i, 'are',lakeshore(V_i, dat))
 # What is the error term? use np.std over all the points known.
