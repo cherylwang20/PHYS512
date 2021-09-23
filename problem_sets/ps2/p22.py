13c38ffbea5a20c951626b31f73d5e87febdcd8b
cherylwang20 <cherylwang815@g.ucla.edu>
2021-09-23 00:43:23 -0400

    1159

--- problem_sets/ps2/p22.py
+++ problem_sets/ps2/p22.py
@@ -6,6 +6,26 @@ def lorentz(x):
 def integrate_adaptive(fun,a,b,tol,extra = None):
     x = np.linspace(a,b,5)
     y = fun(x)
-    dx = (a - b)/(len(x)-1)
-    area = dx*(y[0])
-    return
+    dx = (b - a) / (len(x) - 1)
+    if extra is None:
+        area1 = 2 * dx * (y[0] + 4 * y[2] + y[4]) / 3
+        area2 = dx * (y[0] + 4 * y[1] + 2 * y[2] + 4 * y[3] + y[4]) / 3
+        err = np.abs(area1 - area2)
+    else:
+        area1 = 2*dx*(extra[0] + 4*fun(x[2]) + extra[2])/3
+        area2 = dx * (extra[0] + 4*fun(x[1]) + 2 * fun(x[2]) + 4 * fun(x[3]) + extra[2])/3
+        err = np.abs(area1 - area2)
+    if err < tol:
+        return area2
+    else:
+        left = integrate_adaptive(fun,a,x[2],tol/2, y)
+        right = integrate_adaptive(fun,x[2],b,tol/2, y)
+    return left+right
+
+a = -100
+b = 100
+ans = integrate_adaptive(lorentz,a,b,1e-7)
+act = np.arctan(b) - np.arctan(a)
+print(ans)
+print(ans - act)
+
