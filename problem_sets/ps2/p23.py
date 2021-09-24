6c2a12e5c2467473f6ba9d4638c567d947a974cd
cherylwang20 <cherylwang815@g.ucla.edu>
2021-09-24 01:18:58 -0400

    0118

--- problem_sets/ps2/p23.py
+++ problem_sets/ps2/p23.py
@@ -1,17 +1,4 @@
 import numpy as np
-import matplotlib.pyplot as plt
-
-inv = 100
-
-x = np.linspace(0.5,1,inv)
-y = np.log2(x)
-
-# rescaling the x to from -1 to 1 to fit the chebyshev polynomial
-x_rescale = np.linspace(-1,1,inv)
-n = np.polynomial.chebyshev.chebfit(x_rescale,y,8)
-che = np.polynomial.chebyshev.chebval(x_rescale,n)
-
-print(np.max(y-che))
 
 def mylog2(x):
     ma_x, exp_x = np.frexp(x)
@@ -23,7 +10,19 @@ def mylog2(x):
     lnx = (ma + exp_x)/(me + exp_e)
     return lnx
 
-x_m= np.linspace(0.1, 10, 20)
-ln_act = np.log(10)
-err = abs(mylog2(10) - ln_act)
-print(err)
+inv = 100
+x = np.linspace(0.5,1,inv)
+y = np.log2(x)
+
+# rescaling the x to from -1 to 1 to fit the chebyshev polynomial
+x_rescale = np.linspace(-1,1,inv)
+n = np.polynomial.chebyshev.chebfit(x_rescale,y,7)
+che = np.polynomial.chebyshev.chebval(x_rescale,n)
+
+print(np.max(y-che))
+
+
+x_m= np.linspace(0.1, 10, 1000)
+ln_act = np.log(x_m)
+err = abs(mylog2(x_m) - ln_act)
+print(err.mean())
