3b63485d2f8a302ae8826359d5fd1a0cfd63e536
cherylwang20 <cherylwang815@g.ucla.edu>
2021-09-24 01:10:53 -0400

    1209

--- problem_sets/ps2/p23.py
+++ problem_sets/ps2/p23.py
@@ -1,15 +1,29 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
+inv = 100
 
-x = np.linspace(0.5,1,100)
+x = np.linspace(0.5,1,inv)
 y = np.log2(x)
 
+# rescaling the x to from -1 to 1 to fit the chebyshev polynomial
+x_rescale = np.linspace(-1,1,inv)
+n = np.polynomial.chebyshev.chebfit(x_rescale,y,8)
+che = np.polynomial.chebyshev.chebval(x_rescale,n)
 
-n = np.polynomial.chebyshev.chebfit(x,y,10)
-che = np.polynomial.chebyshev.chebval(x,n)
+print(np.max(y-che))
 
-print(np.std(abs(y-che)))
+def mylog2(x):
+    ma_x, exp_x = np.frexp(x)
+    ma_e, exp_e = np.frexp(np.e)
 
+    ma = np.polynomial.chebyshev.chebval(ma_x,n)
+    me = np.polynomial.chebyshev.chebval(ma_e,n)
 
-# print(che)
+    lnx = (ma + exp_x)/(me + exp_e)
+    return lnx
+
+x_m= np.linspace(0.1, 10, 20)
+ln_act = np.log(10)
+err = abs(mylog2(10) - ln_act)
+print(err)
