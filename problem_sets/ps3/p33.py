a53c67a46f6e3042895a5c7b17ae1715f08b0c1b
cherylwang20 <cherylwang815@g.ucla.edu>
2021-10-07 00:45:21 -0400

    1245

--- problem_sets/ps3/p33.py
+++ problem_sets/ps3/p33.py
@@ -1,40 +1,54 @@
+import matplotlib.pyplot as plt
 import numpy as np
 
 dat = np.loadtxt('dish_zenith.txt')
 
-# position coordinate in mm
+# extract position coordinate in mm
 x = dat[:,0]
 y = dat[:,1]
 z = dat[:,2]
 
+# create a matrix and define its parameters
 A = np.zeros([len(x),4])
-
 A[:,0] =  x**2 + y**2
 A[:,1] = -x
 A[:,2] = -y
 A[:,3] = [1.]*len(x)
-
 A = np.matrix(A)
 
 d = np.matrix(z).transpose()
 
+# carry out the fit using SVD
 u,s,v = np.linalg.svd(A,False)
 sinv = np.matrix(np.diag(1.0/s))
 fitp = v.transpose()*sinv*(u.transpose()*d)
 
 fitp = np.squeeze(np.asarray(fitp))
-print(fitp)
+print(f'The best fit parameters are: p = {fitp[0]:.3e}, b = {fitp[1]:.3e}, c = {fitp[2]:.3e}, d = {fitp[3]:.3e}')
 
+# convert the matrix element into the parameters of desire
 q, b, c, p = fitp
 a = q
 x0 = b/2/a
 y0 = c/2/a
 z0 = p - a*x0**2 - a*y0**2
-print(a,x0,y0,z0)
 
+print(f'The best fit parameters are: a = {a:.3e}, x_0 = {x0:.3f}, y_0 = {y0:.3f}, z_0 = {z0:.3f}')
+
+
+# compute our z
 zn = a*((x-x0)**2 + (y - y0)**2) + z0
 
-noise = np.std(abs(z - zn))
+
+# compare with the original value of z and acquire the noise
+noise = np.std(z - zn)
+
+# plot the noise of the system
+plt.plot((z-zn))
+plt.ylabel('Noise')
+plt.savefig("Noise.png",dpi=300, bbox_inches = "tight")
+# plt.show()
+
 N = np.eye(len(x))*noise**2
 Ninv = np.eye(len(x))*noise**(-2)
 mat=A.T@Ninv@A
@@ -44,6 +58,17 @@ err = np.sqrt(np.diag(errs))
 derrs = A@errs@A.T
 model_sig = np.sqrt(np.diag(derrs))
 
-print(noise)
-print('The uncertainty of a is', err[0])
+print(f'The noise of the system is: {noise:.3f}')
+print(f'The uncertainty of a is: {err[0]:.3e}')
+
+# calculating the focal length, derivation in doc
+r = np.sqrt(-z0/a)+y0
+f = abs(r**2/4/z0)/1000 # in m
+
+# error bar
+err_bar =  (r/2/z0*(noise) - r**2/4/z0**2*(noise))/1000
+
+print(f'The focal length is:  {f:.4f} +/- {-err_bar:.3f} m')
+
+
 
