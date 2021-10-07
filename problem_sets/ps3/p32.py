c305aa43db9d1ad87db495d884a9ca9d9900b79b
cherylwang20 <cherylwang815@g.ucla.edu>
2021-10-06 21:40:51 -0400

    0840

--- problem_sets/ps3/p32.py
+++ problem_sets/ps3/p32.py
@@ -15,12 +15,13 @@ ytm = 3.154*10**13/160
 
 #converted to timesteps
 hl = [10**21, 24*dtm, 6.7*htm, 245500*ytm, 75380*ytm, 1600*ytm,
-             38235*dtm, 3.1*mtm, 26.8*mtm, 19.9*mtm, 164.3/160, 22.3*ytm,
+             3.8235*dtm, 3.1*mtm, 26.8*mtm, 19.9*mtm, 164.3/160, 22.3*ytm,
              5.015*ytm, 138.376*dtm]
 
 
 print(len(hl))
 
+# define the function that runs through the lifetime of each step
 def fun(x,y,half_life = hl ):
     dydx = np.zeros(len(half_life)+1)
     dydx[0] = -y[0]/half_life[0]
@@ -29,23 +30,27 @@ def fun(x,y,half_life = hl ):
     dydx[-1] = y[-2]/half_life[-2]
     return dydx
 
+#setting up the initial conditions of the sets of ODEs
 t0 = 0
 t1 = 2*10.**21
 y0 = [0]*(len(hl)+1)
 y0[0] = 1
 
+# setting our timesteps to 1000 points.
 t_ev = np.logspace(0, np.log10(0.9*t1), num = 1000)
-print(t_ev)
 
 t_s = time.time()
 # ans_rk4 = integrate.solve_ivp(fun,[t0,t1],y0,t_eval=t_ev, method = 'RK45')
+# ans_rk2 = integrate.solve_ivp(fun,[t0,t1],y0 ,method = 'RK23')
+# both of those methods take too long to run
 ans_radau = integrate.solve_ivp(fun, [t0,t1], y0 , t_eval=t_ev, method = 'Radau')
-#ans_radau = integrate.solve_ivp(fun,[t0,t1],y0 ,method = 'RK23')
+
 t_en = time.time()
 
-#print(ans_radau.t)
-# print(ans_radau.y[0,-1],ans_radau.y[0],ans_radau.y[14],ans_radau.t)
-# print(t_e - t_s)
+
+print(t_en - t_s)
+
+# extract the number from the output of the ODE solver
 u238 , pb206 = ans_radau.y[0],ans_radau.y[14]
 th230, u234= ans_radau.y[4], ans_radau.y[3]
 t = ans_radau.t
@@ -53,17 +58,25 @@ t = ans_radau.t
 r1 = pb206/u238
 r2 = th230/u234
 
-
-plt.plot(t,r1)
-plt.plot(t,u238)
-plt.plot(t,pb206,'-.')
+# the last 100 points are of our interest
+plt.plot(t[900:],r1[900:],label = 'Pb206/U238')
+plt.plot(t[900:],u238[900:],label = 'U238')
+plt.plot(t[900:],pb206[900:],'-.', label = 'Pb206')
+plt.legend()
+plt.xscale('log')
+plt.xlabel('Timesteps')
+plt.ylabel('Ratio')
 plt.savefig("upb.png",dpi=300, bbox_inches = "tight")
 plt.show()
 
-plt.plot(t[:-150],r2[:-150])
-plt.plot(t,th230)
-plt.plot(t, u234,'-.')
-plt.yscale('log')
+# plot the region of interest
+plt.plot(t[600:-150],r2[600:-150],label = 'Th230/U234')
+#plt.plot(t[600:-150],th230[600:-150])
+#plt.plot(t[600:-150], u234[600:-150],'-.')
+#plt.yscale('log')
 plt.xscale('log')
+plt.xlabel('Timesteps')
+plt.ylabel('Ratio')
+plt.legend()
 plt.savefig("thu.png",dpi=300, bbox_inches = "tight")
 plt.show()
