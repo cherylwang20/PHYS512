ad13963912dfa652e53d49ae0876fca5b8e645ad
cherylwang20 <cherylwang815@g.ucla.edu>
2021-10-10 14:20:42 -0400

    0212

--- problem_sets/ps4/p41.py
+++ problem_sets/ps4/p41.py
@@ -1,6 +1,11 @@
 import numpy as np
+from matplotlib import pyplot as plt
+import camb
+from camb import model, initialpower
+
 
 dat = np.loadtxt('COM_PowerSpect_CMB-TT-full_R3.01.txt')
 
 multi = dat[:,0]; var = dat[:,1];
 lowsig = dat[:,2]; highsig = dat[:,3];
+
