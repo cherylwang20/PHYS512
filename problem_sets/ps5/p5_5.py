5cdd40535b1945cb1bb9cc03978b0a3baf264037
cherylwang20 <cherylwang815@g.ucla.edu>
2021-10-30 15:48:52 -0400

    1122

--- problem_sets/ps5/p5_5.py
+++ problem_sets/ps5/p5_5.py
@@ -2,55 +2,64 @@ import numpy as np
 from matplotlib import pyplot as plt
 from scipy.fftpack import fft, ifft
 
-k = 1/3
-fs = 1000
-nCyl = 5
-oversampleRate = 30
+N = 1024
+k = 10.4
+x = np.arange(N)
+y = np.sin(2*np.pi*k*x/N)
+y_ft = np.abs(np.fft.fft(y))
 
-x = np.arange(0,nCyl/k - 1/fs, 1/fs )
-y = np.sin(2*np.pi*k*x)
-
-yft = np.abs(np.fft.fftshift(np.fft.fft(y)))
-yft = yft/np.sum(yft)
+fn = np.arange(0, 1, 1/N)
 
 
-ke  = np.arange(0,fs)
+ke  = x
 J = np.complex(0, 1)
-f1 = [0]*fs
-f2 = [0]*fs
+f1 = [0]*N
+f2 = [0]*N
 for i in range(len(ke)):
-    f1[i] = (1 - np.exp(-2*np.pi*J*(ke[i] + k)))/(1-np.exp(-2*np.pi*J*(ke[i] + k)/(fs+1)))
-    f2[i] = (1 - np.exp(-2 * np.pi * J * (ke[i] - k))) / (1 - np.exp(-2 * np.pi * J * (ke[i] - k) /(fs+1)))
+    f1[i] = (1 - np.exp(-2*np.pi*J*(ke[i] + k)))/(1-np.exp(-2*np.pi*J*(ke[i] + k)/(N)))
+    f2[i] = (1 - np.exp(-2 * np.pi * J * (ke[i] - k))) / (1 - np.exp(-2 * np.pi * J * (ke[i] - k) /(N)))
 f = np.abs((np.array(f1) - np.array(f2))/(2*J))
-f = f/np.sum(f)
+#f = f/np.sum(f)
 
-
-
-fre = np.arange(0, len(x))/len(x)
-plt.plot(fre, yft,label = 'true')
-#plt.plot(np.fft.fftshift(f),label = 'analytic')
-# plt.plot(np.arange(0,N), yftt)
-# plt.plot(np.arange(0,N), f)
+# plt.plot(fn, y_ft,label = 'True')
+# plt.plot(fn, f,label = 'Analytic')
+# plt.xlabel('Frequency')
+# plt.ylabel('Amplitude')
 # plt.legend()
-# plt.xlim(-3000,5000)
-# plt.title(f'Non-Integer Sine Function with N = {N} data points')
-# #plt.savefig(f'Analyic_Sine_{N}.png',dpi = 300, bbox_inches = 'tight')
-plt.show()
-#
-#
-# win=0.5-0.5*np.cos(2*x*np.pi/N)
-# f1_w = [0]*N
-# f2_w = [0]*N
-# for i in range(len(ke)):
-#     f1_w[i] = (1 - np.exp(-2*np.pi*J*(ke[i] + k)))/(1-np.exp(-2*np.pi*J*(ke[i] + k)/(N+1)))*win
-#     f2_w[i] = (1 - np.exp(-2 * np.pi * J * (ke[i] - k))) / (1 - np.exp(-2 * np.pi * J * (ke[i] - k) /(N+1)))*win
-# f_win = np.abs((np.array(f1) - np.array(f2))/(2*J))
-# f_win = f_win/np.sum(f_win)
-#
-# plt.plot(x,yft,label = 'true')
-# plt.plot(x,np.fft.fftshift(f_win),label = 'analytic')
-# plt.legend()
-# plt.xlim(4,5)
-# plt.title(f'Non-Integer Sine Function with N = {N} data points')
-# plt.savefig(f'Analyic_Sine_{N}_win.png',dpi = 300, bbox_inches = 'tight')
+# plt.savefig(f'Analyic_Sine_k = {k}_N = {N}.png',dpi = 300, bbox_inches = 'tight')
 # plt.show()
+
+res = np.std(np.abs(f - y_ft))
+
+print(f'The residue between the functions are: {res}, with N = {N}.' )
+# the closer to the end, the larger the residue.
+# plt.plot(fn, f - y_ft)
+# plt.title('Residual between the two functions')
+# plt.show()
+
+win = 0.5 - 0.5*np.cos(2*np.pi*x/N)
+y_win = y*win
+y_win_ft = np.abs(np.fft.fft(y_win))
+
+fa = [0]*N
+fb = [0]*N
+fc = [0]*N
+fd = [0]*N
+for i in range(len(ke)):
+    fa[i] = (1 - np.exp(-2 * np.pi * J * (ke[i] - k - 1))) / (1- np.exp( -2 * np.pi * J * (ke[i] - k - 1) / N))
+    fb[i] = (1 - np.exp(-2 * np.pi * J * (ke[i] - k + 1))) / (1 - np.exp(-2 * np.pi * J * (ke[i] - k + 1) / N))
+    fc[i] = (1 - np.exp(-2 * np.pi * J * (ke[i] + k - 1))) / (1 - np.exp(-2 * np.pi * J * (ke[i] + k - 1) / N))
+    fb[i] = (1 - np.exp(-2 * np.pi * J * (ke[i] + k + 1))) / (1 - np.exp(-2 * np.pi * J * (ke[i] + k + 1) / N))
+f_awin = (np.array(fa) + np.array(fb) - np.array(fc) - np.array(fd))/(8*J)
+f_aawin = (np.array(f1) - np.array(f2))/(4*J)
+f_awin = np.abs(f_aawin - f_awin)
+
+plt.plot(fn, f_awin)
+#plt.plot(fn, y_ft)
+plt.show()
+
+fx = [0]*N
+fy = [0]*N
+for i in range(len(ke)):
+    fx[i] = (1 - np.exp(-2 * np.pi * J * (ke[i] + k + 1))) / (1- np.exp( -2 * np.pi * J * (ke[i] + k + 1) / N))
+    fy[i] = (1 - np.exp(-2 * np.pi * J * (ke[i] - k + 1))) / (1 - np.exp(-2 * np.pi * J * (ke[i] - k + 1) / N))
--- problem_sets/ps5/p5_5_new.py
+++ problem_sets/ps5/p5_5_new.py
@@ -10,7 +10,9 @@ x = n/fs
 
 y = np.sin(2*np.pi*k*x/N)
 
-yft = np.abs(np.fft.fft(y))
+NFFT = 1024
+nVAL = np.arange(0,NFFT)
+yft = np.abs(fft(y,NFFT))
 yft = yft/np.sum(yft)
 fre = x*fs/N
 
@@ -26,33 +28,33 @@ f = f_1/np.sum(f_1)
 
 
 
-plt.plot(fre, yft,label = 'true')
-plt.plot(fre,f,label = 'analytic')
+plt.plot(nVAL, np.fft.fftshift(yft),label = 'true')
+#plt.plot(fre,f,label = 'analytic')
 plt.legend()
 plt.title(f'Non-Integer Sine Function with k = {k}')
-plt.savefig(f'Analyic_Sine_{k}.png',dpi = 300, bbox_inches = 'tight')
+#plt.savefig(f'Analyic_Sine_{k}.png',dpi = 300, bbox_inches = 'tight')
 plt.show()
 
-
-f1_w = [0]*N
-f2_w = [0]*N
-for i in range(len(ke)):
-    f1_w[i] = (1 - np.exp(4*np.pi*J*(k - ke[i])))/(1-np.exp(4*np.pi*J*(k - ke[i])/(N)))
-    f2_w[i] = (1 - np.exp(-4 * np.pi * J * (ke[i] + k))) / (1 - np.exp(-4 * np.pi * J * (ke[i] + k) /(N)))
-f_w = np.abs((np.array(f1) - np.array(f2))/(2*J))/4
-
-
-f_final = f_1/2 + f_w/4
-f_final = f_final/np.sum(f_final)
-plt.plot(fre,yft,label = 'true')
-plt.plot(fre,np.fft.fftshift(f_final),label = 'analytic')
-plt.legend()
-plt.title(f'Non-Integer Sine Function with N = {N} data points')
-plt.savefig(f'Analyic_Sine_{N}_win.png',dpi = 300, bbox_inches = 'tight')
-plt.show()
-
-w = 0.5 - 0.5*np.cos(2*np.pi*x/N)
-w_ft = np.fft.fft(w)
-print(w_ft[0:2])
-plt.plot(fre,w_ft)
-plt.show()
+#
+# f1_w = [0]*N
+# f2_w = [0]*N
+# for i in range(len(ke)):
+#     f1_w[i] = (1 - np.exp(4*np.pi*J*(k - ke[i])))/(1-np.exp(4*np.pi*J*(k - ke[i])/(N)))
+#     f2_w[i] = (1 - np.exp(-4 * np.pi * J * (ke[i] + k))) / (1 - np.exp(-4 * np.pi * J * (ke[i] + k) /(N)))
+# f_w = np.abs((np.array(f1) - np.array(f2))/(2*J))/4
+#
+#
+# f_final = f_1/2 + f_w/4
+# f_final = f_final/np.sum(f_final)
+# plt.plot(fre,yft,label = 'true')
+# plt.plot(fre,np.fft.fftshift(f_final),label = 'analytic')
+# plt.legend()
+# plt.title(f'Non-Integer Sine Function with N = {N} data points')
+# plt.savefig(f'Analyic_Sine_{N}_win.png',dpi = 300, bbox_inches = 'tight')
+# plt.show()
+#
+# w = 0.5 - 0.5*np.cos(2*np.pi*x/N)
+# w_ft = np.fft.fft(w)
+# print(w_ft[0:2])
+# plt.plot(fre,w_ft)
+# plt.show()
