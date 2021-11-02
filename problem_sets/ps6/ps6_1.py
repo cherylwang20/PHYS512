496998aa5ca0c7e89ad1cab391ffe4c5f4fe5163
cherylwang20 <cherylwang815@g.ucla.edu>
2021-11-01 20:42:23 -0400

    0842

---
+++ problem_sets/ps6/ps6_1.py
@@ -0,0 +1,11 @@
+import numpy as np
+import h5py
+import glob
+import json
+
+exec(open("simple_read_ligo.py").read())
+f = open(r'C:\Users\wangc\PHYS512\problem_sets\ps6\LOSC_Event_tutorial\BBH_events_v3.json',)
+
+data = json.load(f)
+
+print(th)
